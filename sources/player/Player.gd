1d4246ea981170354cdf02879fc15959f1dcd8ca
Pippi <tienlocnguyen.ac@gmail.com>
2020-02-16 21:45:49 +0700

    - Fix warning - Fix issue when press left-right at the same time - Remove test scene

--- sources/player/Player.gd
+++ sources/player/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 
+class_name Player
+
 signal death
 
 export var Gravity: int = 500
@@ -39,18 +41,17 @@ func _physics_process(delta):
 	var crouch = Input.is_action_pressed("ui_down")
 	var stop = true
 	
-	if run_left:
+	if run_left and run_right:
+		# if both left and right is pressed, just stop
+		pass
+	elif run_left:
 		if velocity.x > -RunMaxSpeed:
 			current_run_force.x -= RunForce
 			stop = false
-		else:
-			print('Reached max speed when run left')
 	elif run_right:
 		if velocity.x < RunMaxSpeed:
 			current_run_force.x += RunForce
 			stop = false
-		else:
-			print('Reached max speed when run right')
 			
 	if stop:
 		var vsign = sign(velocity.x)
@@ -70,7 +71,6 @@ func _physics_process(delta):
 
 
 	if crouch and not crouching:
-		print('Crouch')
 		crouching = true
 		stand_time = 0
 		$AnimationPlayer.play("Crouch")
@@ -78,37 +78,34 @@ func _physics_process(delta):
 	if jump:
 		if crouching:
 			if can_stand:
-				print('Stand')
 				$AnimationPlayer.play("Standup")
 				crouching = false
 				stand_time += delta
 			else:
 				print('Can not stand in pipe')
 		elif not jumping and stand_time > StandBouncingTime:
-			print('Jump')
 			velocity.y = -JumpSpeed
 			jumping = true
 		elif stand_time < StandBouncingTime:
 			# just stand after crouch, need more time to stand before jump
-			print('Stand time: ', stand_time)
 			stand_time += delta
 
 
-func _on_StandShape_body_shape_entered(body_id, body, body_shape, area_shape):
+func _on_StandShape_body_entered(body):
 	# TODO check if collide with world instead of other part
+	assert(body)
 	stand_shape_collide_time += 1
 	can_stand = false
-	print('Enter stand shape: ', stand_shape_collide_time)
 
 
-func _on_StandShape_body_shape_exited(body_id, body, body_shape, area_shape):
+func _on_StandShape_body_exited(body):
 	# TODO check if collide with world instead of other part
+	assert(body)
 	stand_shape_collide_time -= 1
 	if stand_shape_collide_time == 0:
 		can_stand = true
-	print('Exit stand shape: ', stand_shape_collide_time)
 
 
-func _on_DeathHole_body_shape_entered(body_id, body, body_shape, area_shape):
-	print('You death')
+func _on_DeathHole_body_entered(body):
+	assert(body)
 	emit_signal("death")
