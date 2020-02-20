fc80d8fdd120bfc9909b310725b7eca3852fb022
Pippi <tienlocnguyen.ac@gmail.com>
2020-02-20 22:29:22 +0700

    Add some comment

--- sources/obstacle/MovingObstacle.gd
+++ sources/obstacle/MovingObstacle.gd
@@ -7,11 +7,15 @@ var is_moving: bool = false
 
 
 func _process(delta):
+	"""Only move when follower/path is set
+	"""
 	if is_moving and follower:
 		follower.offset += speed * delta
 
 
 func attach_to_path(path: Path2D):
+	"""Create PathFollow2D and make it follow the path
+	"""
 	follower = PathFollow2D.new()
 	follower.loop = false
 	follower.add_child(self)
