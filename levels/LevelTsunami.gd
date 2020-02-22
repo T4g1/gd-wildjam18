c9d8da832325dd821eebb056b246943b8f8f095c
T4g1 <thomasvangysegem@gmail.com>
2020-02-22 18:31:56 +0100

    [fix] Last level has end trigger

--- levels/LevelTsunami.gd
+++ levels/LevelTsunami.gd
@@ -1,5 +1,18 @@
-extends "res://sources/level/Level.gd"
+extends "res://sources/levels/LevelTornado.gd"
+
+onready var tornado = $TornadoPath/TornadoFollow/Tornado
 
 
 func _ready():
+	for spawn in get_tree().get_nodes_in_group("spawn"):
+		assert(spawn.connect("spawn_left", self, "_on_spawn_left") == OK)
+
 	yield(start_dialog($UI/Introduction), "completed")
+
+
+func _on_spawn_left(spawn) -> void:
+	"""
+	Enable the tornado when player leaves spawn
+	"""
+	if spawn == Utils.get_game().get_active_spawn():
+		tornado.enable()
