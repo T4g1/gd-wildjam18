33b3c750e167f1219f89813d78f2c55ed1b56a61
T4g1 <thomasvangysegem@gmail.com>
2020-02-22 15:33:04 +0100

    [add] Level 5 basic entities

--- levels/LevelShelter.gd
+++ levels/LevelShelter.gd
@@ -1,10 +1,21 @@
 """
 Specific logic for the level where player open the shelter
+
+In this level player must open steel door solving a short puzzle
 """
 extends "res://sources/level/Level.gd"
 
 
 func _ready():
-	# TODO
-	yield(get_tree(), "idle_frame")
-	end()
+	assert($SteelDoor.connect("interacted", self, "on_door_interacted") == OK)
+	assert($Generator.connect("interacted", self, "on_generator_interacted") == OK)
+	assert($Keypad.connect("interacted", self, "on_keypad_interacted") == OK)
+	assert($Board.connect("interacted", self, "on_board_interacted") == OK)
+
+	VisualServer.set_default_clear_color(Color.black)
+
+	introduction()
+
+
+func introduction() -> void:
+	yield(start_dialog($UI/Introduction), "completed")
