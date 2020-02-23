2822b0b14e84b7efb86b904e1a8ab890bc2bdfa5
T4g1 <thomasvangysegem@gmail.com>
2020-02-23 10:32:45 +0100

    [enh] QWERTY and AZERTY support, all controls with direction keys (down for interction)

--- sources/ui/dialog/SpeechBubble.gd
+++ sources/ui/dialog/SpeechBubble.gd
@@ -82,7 +82,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if not visible:
 		return
 
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("ui_accept") or event.is_action_pressed("interact"):
 		next()
 
 
